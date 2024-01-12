b9705769e083c746fc9e0eff948a8f706246e36c
xanaccs <73348553+xanaccs@users.noreply.github.com>
2024-01-12 00:23:10 +0000

    Prefil

---
+++ Online-Auctions-LI4/Controllers/ProfileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Online_Auctions_LI4.Models;
+
+
+namespace Online_Auctions_LI4.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private string ImageUrl;
+
+        public string GetImageUrl()
+        {
+            return ImageUrl;
+        }
+
+        public ActionResult Index()
+        {
+            var viewModel = new ProfileModel();
+            return View(viewModel);
+        }
+
+        public IActionResult Index(string imageUrl)
+        {
+            ProfileModel profileModel = new ProfileModel
+            {
+                ImageUrl = "https://example.com/default-profile-picture.jpg",
+                Name = "Jeronimo Antonio",
+                Username = "jermsant",
+                Location = "Braga, Portugal"
+            };
+
+            return View(profileModel);
+        }
+    }
+}
---
+++ Online-Auctions-LI4/Models/ProfileModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Online_Auctions_LI4.Models
+{
+    public class ProfileModel
+    {
+        internal string ImageUrl;
+
+        [Key]
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(20, ErrorMessage = "Username cannot be longer than 20 characters.")]
+        public string Username { get; set; }
+
+        [StringLength(50, ErrorMessage = "Location cannot be longer than 50 characters.")]
+        public string Location { get; set; }
+    }
+}
